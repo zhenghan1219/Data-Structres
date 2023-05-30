77aef4033a4c2cdb10cc96087cb007d53b6b344f
zhenghan1219 <zhenghan1219@163.com>
2023-05-30 12:38:53 +0800

    数据结构

---
+++ src/main/java/com/zzh/datastructres/DataStructresApplication.java
@@ -0,0 +1,13 @@
+package com.zzh.datastructres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzh/datastructres/DataStructresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzh.datastructres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
